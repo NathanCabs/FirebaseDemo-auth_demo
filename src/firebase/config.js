cce7854fe9515797a215415211073b9dc93272f1
Nathaniel Caballero <nathaniel.caballero@ciit.edu.ph>
2024-07-13 11:37:38 +0800

    updated config

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -3,12 +3,13 @@ import {getFirestore} from 'firebase/firestore'
 import {getAuth} from "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCRwWd1E7JJiSuHK-J3FRXj8wjK3-WO84E",
-    authDomain: "reactdemojuly2024.firebaseapp.com",
-    projectId: "reactdemojuly2024",
-    storageBucket: "reactdemojuly2024.appspot.com",
-    messagingSenderId: "140474381397",
-    appId: "1:140474381397:web:314537f93c0cd60f1090dc"
+  apiKey: "AIzaSyDWKND510GjHx8QbeH1UdAXMF2ibnuBUCo",
+  authDomain: "fir-project-915b0.firebaseapp.com",
+  projectId: "fir-project-915b0",
+  storageBucket: "fir-project-915b0.appspot.com",
+  messagingSenderId: "330676880081",
+  appId: "1:330676880081:web:b696e3ab5c39a2580a3bf0",
+  measurementId: "G-XDZZKZQMGD"
 };
 
   initializeApp(firebaseConfig);
